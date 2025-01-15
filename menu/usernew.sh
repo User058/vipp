8b5d640d9de2c3193e92e1daf219eb12aa51ed00
User058 <110759720+User058@users.noreply.github.com>
2025-01-15 23:13:42 +0300

    Update usernew.sh

--- menu/usernew.sh
+++ menu/usernew.sh
@@ -100,9 +100,9 @@ fi
 portsshws=`cat /root/log-install.txt | grep -w "SSH Websocket" | cut -d: -f2 | awk '{print $1}'`
 wsssl=`cat /root/log-install.txt | grep -w "SSH SSL Websocket" | cut -d: -f2 | awk '{print $1}'`
 
-echo -e "◇━━━━━━━━━━━━━━━━━◇"
-echo -e "   ADD SSH ACCOUNT"
-echo -e "◇━━━━━━━━━━━━━━━━━◇"
+cho -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\\E[0;41;36m      ADD SSH Account		      \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 read -p "Username       : " Login
 read -p "Password       : " Pass
 read -p "Expired (Days) : " masaaktif
