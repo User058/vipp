7d2e077441a8866e6dc68c3628a24021082e43a2
User058 <110759720+User058@users.noreply.github.com>
2025-01-16 08:48:10 +0300

    Update usernew.sh

--- menu/usernew.sh
+++ menu/usernew.sh
@@ -101,11 +101,11 @@ portsshws=`cat /root/log-install.txt | grep -w "SSH Websocket" | cut -d: -f2 | a
 wsssl=`cat /root/log-install.txt | grep -w "SSH SSL Websocket" | cut -d: -f2 | awk '{print $1}'`
 
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\\E[0;41;36m      ADD SSH Account		       \E[0m"
+echo -e "\\E[0;41;36m      ADD SSH Account              \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -p "Username       : " Login
-read -p "Password       : " Pass
-read -p "Expired (Days) : " masaaktif
+read -p "Username : " Login
+read -p "Password : " Pass
+read -p "Expirey  : " masaaktif
 
 IP=$(curl -sS ifconfig.me);
 sleep 1
